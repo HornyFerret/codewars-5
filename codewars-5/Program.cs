53ad50472a70fb943f3467148ef50a896272de23
HornyFerret <ombro.de.morto@gmail.com>
2021-07-10 16:32:05 +0300

    Добавьте файлы проекта.

---
+++ codewars-5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace codewars_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double time = 6.7;
+            Console.WriteLine(time);
+
+            int w = Litres(time);
+            Console.WriteLine(w);
+        }
+        public static int Litres(double time)
+        {
+            //The fun begins here.
+            int liters = 0;
+            if (time >= 0)
+            {
+                liters = (int)(time * 0.5);
+            }
+            return liters;
+        }
+    }
+}
